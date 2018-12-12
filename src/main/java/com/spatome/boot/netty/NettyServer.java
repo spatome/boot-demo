f373996f374d75597591257c4b65ae77799ae22d
spatome <spatome@126.com>
2018-12-12 14:47:36 +0800

    第一次

---
+++ src/main/java/com/spatome/boot/netty/NettyServer.java
@@ -0,0 +1,47 @@
+package com.spatome.boot.netty;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class NettyServer {
+
+	private final int port;
+
+	public NettyServer(int port) {
+		this.port = port;
+	}
+
+	public void start(){
+		EventLoopGroup bossGroup = new NioEventLoopGroup();		//用来接收进来的连接
+		EventLoopGroup workerGroup = new NioEventLoopGroup();	//用来处理已经被接收的连接
+		try {
+			ServerBootstrap serverBootstrap = new ServerBootstrap();
+			serverBootstrap.group(bossGroup, workerGroup);
+			serverBootstrap.channel(NioServerSocketChannel.class);	//这里告诉Channel如何接收新的连接
+			serverBootstrap.option(ChannelOption.SO_BACKLOG, 1024);
+			serverBootstrap.handler(new LoggingHandler(LogLevel.INFO));
+			serverBootstrap.childOption(ChannelOption.TCP_NODELAY, true);
+			serverBootstrap.childOption(ChannelOption.SO_KEEPALIVE, true);
+			serverBootstrap.childHandler(new NettyServerInitalizer());
+			// 绑定端口,开始接收进来的连接
+			ChannelFuture channelFuture = serverBootstrap.bind(port).sync();
+			log.info("==>netty服务启动: [port:" + port + "]");
+			// 等待服务器socket关闭
+			channelFuture.channel().closeFuture().sync();
+		}catch (Exception e) {
+			log.error("==>netty服务启动异常" + e.getMessage());
+		}finally {
+			workerGroup.shutdownGracefully();
+			bossGroup.shutdownGracefully();
+		}
+	}
+
+}
--- src/main/java/com/spatome/boot/netty/NettyServerHandler.java
+++ src/main/java/com/spatome/boot/netty/NettyServerHandler.java
@@ -4,6 +4,7 @@ import java.net.InetAddress;
 
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.util.ReferenceCountUtil;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -11,29 +12,36 @@ public class NettyServerHandler extends SimpleChannelInboundHandler<String> {
 
 	@Override
 	protected void channelRead0(ChannelHandlerContext ctx, String msg) throws Exception {
-		String clientId = ctx.channel().id().asLongText();
-		log.info("收到clientId:{},消息:{}", clientId, msg);
+		try {
+			String clientId = ctx.channel().id().asLongText();
+			log.info("==>收到clientId:{},消息:{}", clientId, msg);
 
-		ctx.channel().writeAndFlush(msg);
+			//测试原值返回
+			ctx.channel().writeAndFlush(msg);
+		}finally {
+			//抛弃收到的数据
+			ReferenceCountUtil.release(msg);
+		}
 	}
 
 	@Override
     public void channelActive(ChannelHandlerContext ctx) throws Exception {
-        log.info("RamoteAddress:{} active!", ctx.channel().remoteAddress());
-        ctx.channel().writeAndFlush( "Welcome to " + InetAddress.getLocalHost().getHostName() + " service!\n");
+        log.info("==>RamoteAddress:{} active!", ctx.channel().remoteAddress());
+        ctx.channel().writeAndFlush( "Welcome to " + InetAddress.getLocalHost().getHostAddress() + " service!\n");
 
         super.channelActive(ctx);
     }
 
     @Override
     public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+    	//当出现异常就关闭连接
         cause.printStackTrace();
         ctx.close();
     }
 
     @Override
     public void channelInactive(ChannelHandlerContext ctx) throws Exception {
-        log.info("\nChannel is disconnected");
+        log.info("==>Channel is disconnected");
         super.channelInactive(ctx);
     }
 }
